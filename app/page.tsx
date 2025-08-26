cd6c2913d79a984d482371e0755ef0494526fe89
jimmypocock <jimmypocock@yahoo.com>
2025-08-26 16:14:25 -0500

    image placement perfect

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,31 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="min-h-screen relative flex items-end">
-      {/* Background image */}
-      <div 
-        className="fixed inset-0 z-0"
-        style={{
-          backgroundImage: 'url(/thinker.png)',
-          backgroundPosition: 'center center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundColor: '#000'
-        }}
-      />
+    <div className="fixed inset-0 bg-black overflow-visible">
+      {/* Image - scaled 300% width, 140% height */}
+      <div className="fixed top-[65%] left-1/4 -translate-x-1/2 -translate-y-1/2 w-[300vw] h-[140vh] pointer-events-none z-0">
+        <Image
+          src="/thinker.png"
+          alt="The Thinker"
+          fill
+          sizes="300vw"
+          className="object-contain"
+          priority
+        />
+      </div>
       
-      {/* Content */}
-      <div className="relative z-10 w-full pb-6 md:pb-12">
-        <div className="w-full max-w-3xl mx-auto px-4">
+      {/* Content overlay */}
+      <div className="relative z-10 w-full h-full flex flex-col lg:flex-row">
+        {/* Spacer for image area on desktop */}
+        <div className="hidden lg:block lg:w-1/2"></div>
+        
+        {/* Text content on the right */}
+        <div className="w-full lg:w-1/2 h-full overflow-y-auto flex px-4 sm:px-8">
+        <div className="py-8">
           <h1 className="text-white text-3xl md:text-6xl mb-6 tracking-widest lowercase">
             jimmy pocock
           </h1>
@@ -67,6 +73,17 @@ export default function Home() {
               songsnips
             </a>
           </p>
+          <p className="text-white text-base md:text-xl mb-2 tracking-wider">
+            currently building{' '}
+            <a 
+              href="https://www.famefitapp.com" 
+              title="famefitapp.com"
+              className="text-[#ff6100] uppercase tracking-widest hover:underline"
+              target="_blank"
+            >
+              famefit
+            </a>
+          </p>
           <p className="text-white text-base md:text-xl mb-6 tracking-wider">
             let&apos;s{' '}
             <a 
@@ -106,6 +123,7 @@ export default function Home() {
               thoughts
             </Link>
           </p>
+        </div>
         </div>
       </div>
     </div>
